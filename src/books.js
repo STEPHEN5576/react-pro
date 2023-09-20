4ff2592509539976acfa6029270b2d4eab3d1fc2
STEPHEN5576 <116451839+STEPHEN5576@users.noreply.github.com>
2023-09-20 15:52:21 +0530

    first commit

---
+++ src/books.js
@@ -0,0 +1,15 @@
+ const bookList = [
+  {
+    title: "The Little Book of Good Things",
+    Author: "Bhavya Doshi",
+    img: "./img/img3.jpg",
+    id: 1,
+  },
+  {
+    title: "The Carpenter",
+    Author: "Jon Gordon",
+    img: "./img/img4.jpg",
+    id: 2,
+  },
+];
+export default bookList;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,90 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// import { queryAllByDisplayValue } from "@testing-library/react";
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+import bookList from './books'
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Books = () => {
+  const getbook = (x)=>{
+    const book = bookList.find((book)=>(book.id === x));
+    console.log(book);
+  }
+
+  return (
+    <section className="books">
+      {/* <EventExamples/> */}
+      {bookList.map((book) => {
+        return (
+          <Book {...book} key={book.id} getbook={getbook} />
+        );
+      })}
+    </section>
+  );
+};
+
+// const EventExamples=()=>{
+//   const handleButtonClick = (e) => {
+//     // e.preventDefault();
+//     // console.log(e);
+//     console.log("handle the button click");
+//   };
+//   // const handleInputForm=(e) =>{
+//   //   console.log(e.target.name);
+//   //   console.log(e.target.value);
+//   //   console.log("handle Input form");
+//   //   // console.log(e.target);
+//   // }
+//   const submissionMail=(e)=>{
+//     e.preventDefault();
+//     // console.log(e);
+//     console.log("Mail has been submitted");
+//   }
+//   return (
+//     <section>
+//       <form >
+//         <h1>Typical File</h1>
+//         <input
+//           type="text"
+//           name="example"
+//           style={{ margin: "1rem 0" }}
+//           // onChange={handleInputForm}
+//           onChange={(e)=> console.log(e.target.value)}
+//         />
+//         <button type="submit" onClick={submissionMail}>
+//           Submit
+//         </button>
+//       </form>
+//       <div>
+//         <button  onClick={handleButtonClick} type="submit">
+//           click me
+//         </button>
+//       </div>
+//     </section>
+//   );
+// }
+
+
+
+const Book = (props) => {
+  const { img, title, Author, getbook, id} = props;
+  // console.log(props);
+  // const display=()=>{
+  //   console.log(title);
+  // }
+  
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{Author.toUpperCase()}</h4>
+      <button onClick={()=> getbook(id)}>Display title</button>
+      
+    </article>
+  );
+};
+
+root.render(<Books></Books>);
